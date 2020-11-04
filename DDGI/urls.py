88492c21006f7f1896b01b7c1097fb2a239c467a
Mukhammadjon <mukhammadjon@2shop.uz>
2020-11-04 18:12:39 +0500

    api doc established

--- DDGI/urls.py
+++ DDGI/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.documentation import include_docs_urls
+
 from insurance.views import MyTokenObtainPairView
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
@@ -12,5 +14,7 @@ urlpatterns = [
     #path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     #path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('token/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('', include('insurance.urls'))
+    path('', include('insurance.urls')),
+    path('docs/', include_docs_urls(title='My API title'))
+
 ]

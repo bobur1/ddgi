1b5ea1305db62332f04f0ce5dc5fa2dd6f907dd2
kodirov257 <abdurahmonkodirov97@gmail.com>
2020-11-12 20:42:35 +0500

    adding policy transfer

--- insurance/controller.py
+++ insurance/controller.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
-from insurance.models import Currency
+from insurance.models import Currency, RegisteredPolises, Branch
 
 def home(request):
     return render(request, "home.html")
@@ -31,4 +31,17 @@ def polis_registration(request):
 def polis_registration_add(request):
     return render(request, "polis_registration/add.html", {
         'policy': None,
+    })
+
+
+def polis_transfer_list(request):
+    return render(request, "polis_transfer/index.html")
+
+
+def polis_transfer(request):
+    polises = RegisteredPolises.objects.all()
+    offices = Branch.objects.all()
+    return render(request, "polis_transfer/transfer.html", {
+        'polises': polises,
+        'offices': offices,
     })

02d1963400c60f426d3b8346ef49e5cd897357da
kodirov257 <abdurahmonkodirov97@gmail.com>
2020-11-15 22:17:46 +0500

    fixed polis retransfer page

--- insurance/controller.py
+++ insurance/controller.py
@@ -1,35 +1,41 @@
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
-from insurance.models import Currency, Policy, InsuranceOffice, PolicySeriesType, User
+from insurance.models import Currency, Policy, InsuranceOffice, PolicySeriesType, User, OfficeWorkers, PolicyTransfers
 
 @login_required
 def home(request):
     return render(request, "home.html")
 
 
+@login_required
 def individual_client(request):
     return render(request, "individual_client/index.html")
 
 
+@login_required
 def individual_client_add(request):
     currency_list = Currency.objects.all()
     return render(request, "individual_client/add.html", {'curr_list': currency_list,
                                                           'name': 'Shohrux'})
 
 
+@login_required
 def legal_client(request):
     return render(request, "legal_client/index.html")
 
 
+@login_required
 def legal_client_add(request):
     return render(request, "legal_client/add.html")
 
 
+@login_required
 def polis_registration(request):
     return render(request, "polis_registration/index.html")
 
 
+@login_required
 def polis_registration_add(request):
     polis_series = PolicySeriesType.objects.all()
     return render(request, "polis_registration/add.html", {
@@ -38,10 +44,12 @@ def polis_registration_add(request):
     })
 
 
+@login_required
 def polis_transfer_list(request):
     return render(request, "polis_transfer/index.html")
 
 
+@login_required
 def polis_transfer(request):
     polises = Policy.objects.filter(policytransfers__to_office__isnull=True)
     offices = InsuranceOffice.objects.all()
@@ -51,20 +59,27 @@ def polis_transfer(request):
     })
 
 
+@login_required
 def polis_retransfer_list(request):
     return render(request, "polis_retransfer/index.html")
 
 
+@login_required
 def polis_retransfer(request):
-    polises = Policy.objects.filter(policytransfers__to_office__isnull=False)
-    users = User.objects.all()
+    office = InsuranceOffice.objects.filter(director=request.user).first()
+    officeWorkers = OfficeWorkers.objects.filter(office=office)
+    polisTransfers = PolicyTransfers.objects.filter(to_office=office)
     return render(request, "polis_retransfer/retransfer.html", {
-        'polises': polises,
-        'users': users,
+        'polisTransfers': polisTransfers,
+        'officeWorkers': officeWorkers,
     })
 
+
+@login_required
 def request (request):
     return render(request, "request/add.html")
 
+
+@login_required
 def spravochnik_bank (request):
     return render(request, "spravochniki/bank/add.html")

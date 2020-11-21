bf12e21f675aa22d0a764f92027ad155cdabc7a1
kodirov257 <abdurahmonkodirov97@gmail.com>
2020-11-21 21:56:32 +0500

    added products

--- insurance/controller.py
+++ insurance/controller.py
@@ -2,7 +2,7 @@ from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
 from insurance.models import Currency, Policy, InsuranceOffice, PolicySeriesType, User, OfficeWorkers, PolicyTransfers, \
-    Vid, IndividualClient, LegalClient, Position, ProductTypeCode, OfficeType, Bank
+    Vid, IndividualClient, LegalClient, Position, ProductTypeCode, OfficeType, Bank, ProductType, ProductField
 
 @login_required
 def home(request):
@@ -120,161 +120,161 @@ def user (request):
 
 @login_required
 def bank(request):
-    return render(request, "spravochniki/bank/index.html")
+    return render(request, "references/bank/index.html")
 
 
 @login_required
 def bank_add(request):
-    return render(request, "spravochniki/bank/add.html")
+    return render(request, "references/bank/add.html")
 
 
 @login_required
 def bank_show(request, id):
     bank = Bank.objects.filter(id=id).first()
-    return render(request, "spravochniki/bank/show.html", { 'bank': bank })
+    return render(request, "references/bank/show.html", { 'bank': bank })
 
 
 @login_required
 def bank_edit(request, id):
     bank = Bank.objects.filter(id=id).first()
-    return render(request, "spravochniki/bank/edit.html", { 'bank': bank })
+    return render(request, "references/bank/edit.html", { 'bank': bank })
 
 
 @login_required
 def currency(request):
-    return render(request, "spravochniki/currency/index.html")
+    return render(request, "references/currency/index.html")
 
 
 @login_required
 def currency_add(request):
-    return render(request, "spravochniki/currency/add.html")
+    return render(request, "references/currency/add.html")
 
 
 @login_required
 def currency_show(request, id):
     currency = Currency.objects.filter(id=id).first()
-    return render(request, "spravochniki/currency/show.html", { 'currency': currency })
+    return render(request, "references/currency/show.html", { 'currency': currency })
 
 
 @login_required
 def currency_edit(request, id):
     currency = Currency.objects.filter(id=id).first()
-    return render(request, "spravochniki/currency/edit.html", { 'currency': currency })
+    return render(request, "references/currency/edit.html", { 'currency': currency })
 
 
 @login_required
 def view(request):
-    return render(request, "spravochniki/view/index.html")
+    return render(request, "references/view/index.html")
 
 
 @login_required
 def view_add(request):
-    return render(request, "spravochniki/view/add.html")
+    return render(request, "references/view/add.html")
 
 
 @login_required
 def view_show(request, id):
     view = Vid.objects.filter(id=id).first()
-    return render(request, "spravochniki/view/show.html", { 'view': view })
+    return render(request, "references/view/show.html", { 'view': view })
 
 
 @login_required
 def view_edit(request, id):
     view = Vid.objects.filter(id=id).first()
-    return render(request, "spravochniki/view/edit.html", { 'view': view })
+    return render(request, "references/view/edit.html", { 'view': view })
 
 
 @login_required
 def klass(request):
-    return render(request, "spravochniki/klass/index.html")
+    return render(request, "references/klass/index.html")
 
 
 @login_required
 def klass_add(request):
-    return render(request, "spravochniki/klass/add.html")
+    return render(request, "references/klass/add.html")
 
 
 @login_required
 def klass_show(request, id):
     klass = ProductTypeCode.objects.filter(id=id).first()
-    return render(request, "spravochniki/klass/show.html", { 'klass': klass })
+    return render(request, "references/klass/show.html", { 'klass': klass })
 
 
 @login_required
 def klass_edit(request, id):
     klass = ProductTypeCode.objects.filter(id=id).first()
-    return render(request, "spravochniki/polis-series/edit.html", { 'klass': klass })
+    return render(request, "references/polis-series/edit.html", { 'klass': klass })
 
 
 @login_required
 def polis_series(request):
-    return render(request, "spravochniki/polis-series/index.html")
+    return render(request, "references/polis-series/index.html")
 
 
 @login_required
 def polis_series_add(request):
-    return render(request, "spravochniki/polis-series/add.html")
+    return render(request, "references/polis-series/add.html")
 
 
 @login_required
 def polis_series_show(request, id):
     polisSeries = PolicySeriesType.objects.filter(id=id).first()
-    return render(request, "spravochniki/polis-series/show.html", { 'polisSeries': polisSeries })
+    return render(request, "references/polis-series/show.html", { 'polisSeries': polisSeries })
 
 
 @login_required
 def polis_series_edit(request, id):
     polisSeries = PolicySeriesType.objects.filter(id=id).first()
-    return render(request, "spravochniki/klass/edit.html", { 'polisSeries': polisSeries })
+    return render(request, "references/klass/edit.html", { 'polisSeries': polisSeries })
 #
 #
 # @login_required
 # def group(request):
-#     return render(request, "spravochniki/view/index.html")
+#     return render(request, "references/view/index.html")
 #
 #
 # @login_required
 # def group_add(request):
-#     return render(request, "spravochniki/group/add.html")
+#     return render(request, "references/group/add.html")
 #
 #
 # @login_required
 # def group_show(request, id):
 #     group = Group.objects.filter(id=id).first()
-#     return render(request, "spravochniki/group/show.html", { 'group': group })
+#     return render(request, "references/group/show.html", { 'group': group })
 #
 #
 # @login_required
 # def group_edit(request, id):
 #     group = Group.objects.filter(id=id).first()
-#     return render(request, "spravochniki/group/edit.html", { 'group': group })
+#     return render(request, "references/group/edit.html", { 'group': group })
 
 
 @login_required
 def position(request):
-    return render(request, "spravochniki/view/index.html")
+    return render(request, "references/view/index.html")
 
 
 @login_required
 def position_add(request):
-    return render(request, "spravochniki/position/add.html")
+    return render(request, "references/position/add.html")
 
 
 @login_required
 def position_show(request, id):
     position = Position.objects.filter(id=id).first()
-    return render(request, "spravochniki/position/show.html", { 'position': position })
+    return render(request, "references/position/show.html", { 'position': position })
 
 
 @login_required
 def position_edit(request, id):
     position = Position.objects.filter(id=id).first()
-    return render(request, "spravochniki/position/edit.html", { 'position': position })
+    return render(request, "references/position/edit.html", { 'position': position })
 
 
 @login_required
 def branch(request):
-    return render(request, "spravochniki/branch/index.html")
+    return render(request, "references/branch/index.html")
 
 
 @login_required
@@ -283,7 +283,7 @@ def branch_add(request):
     officeTypes = OfficeType.objects.all()
     parentBranches = InsuranceOffice.objects.all()
     # regions = Region.objects.all()
-    return render(request, "spravochniki/branch/add.html", {
+    return render(request, "references/branch/add.html", {
         'users': users,
         'officeTypes': officeTypes,
         'parentBranches': parentBranches,
@@ -293,7 +293,7 @@ def branch_add(request):
 @login_required
 def branch_show(request, id):
     branch = InsuranceOffice.objects.filter(id=id).first()
-    return render(request, "spravochniki/branch/show.html", { 'branch': branch })
+    return render(request, "references/branch/show.html", { 'branch': branch })
 
 
 @login_required
@@ -302,9 +302,31 @@ def branch_edit(request, id):
     users = User.objects.get()
     officeTypes = OfficeType.objects.all()
     parentBranches = InsuranceOffice.objects.exclude(id=id)
-    return render(request, "spravochniki/branch/edit.html", {
+    return render(request, "references/branch/edit.html", {
         'branch': branch,
         'users': users,
         'officeTypes': officeTypes,
         'parentBranches': parentBranches,
     })
+
+
+@login_required
+def product(request):
+    return render(request, "product/index.html")
+
+
+@login_required
+def product_add(request):
+    return render(request, "product/add.html")
+
+
+@login_required
+def product_show(request, id):
+    product = ProductType.objects.filter(id=id).first()
+    return render(request, "product/show.html", { 'product': product })
+
+
+@login_required
+def product_edit(request, id):
+    product = ProductType.objects.filter(id=id).first()
+    return render(request, "product/edit.html", { 'product': product })

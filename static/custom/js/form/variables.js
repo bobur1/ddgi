1c040ba5298cf7009a74deaa151566a1775c5c70
kodirov257 <abdurahmonkodirov97@gmail.com>
2020-11-30 22:52:34 +0500

    adding to forms

---
+++ static/custom/js/form/variables.js
@@ -0,0 +1,49 @@
+let insurerModal = $('#insurer-modal');
+let beneficiaryModal = $('#beneficiary-modal');
+let pledgerModal = $('#pledger-modal');
+let productFieldModal = $('#product-field-modal-0');
+
+let insurerModalButton = $('#insurer-modal-button');
+let beneficiaryModalButton = $('#beneficiary-modal-button');
+let pledgerModalButton = $('#pledger-modal-button');
+
+let insurerClose = $('#insurer-close');
+let beneficiaryClose = $('#beneficiary-close');
+let pledgerClose = $('#pledger-close');
+
+let insurerForm = $('#insurer-form');
+let beneficiaryForm = $('#beneficiary-form');
+let pledgerForm = $('#pledger-form');
+
+let insurerFormButton = $('#insurer-form-button');
+let beneficiaryFormButton = $('#beneficiary-form-button');
+let pledgerFormButton = $('#pledger-form-button');
+
+let insurerId = $('#insurer_id');
+let beneficiaryId = $('#beneficiary_id');
+let pledgerId = $('#pledger_id');
+
+let beneficiaryCardBody = $('#beneficiary-card-body');
+let pledgerCardBody = $('#pledger-card-body');
+
+let formContainer = $('#form-container');
+let agreementContainer = $('#agreement-container');
+let polisContainer = $('#polis-container');
+let productId = $('#product-id');
+let individualClientRow = $('#individual-client-row');
+let legalClientRow = $('#legal-client-row');
+let insurerName = $('#insurer-name');
+let submitButton = $('#submit-button');
+
+let productFieldsForm = $('#product-fields');
+let clientProductForm = $('#client-product-form');
+let paymentTermsForm = $('#payment-terms-form');
+let anketaFieldsForm = $('#anketa-fields');
+
+let polises = $('.polises');
+
+let product = null;
+let individualClient = true;
+
+let paymentTypeFieldNumber = 0;
+let productFieldNumber = 0;
